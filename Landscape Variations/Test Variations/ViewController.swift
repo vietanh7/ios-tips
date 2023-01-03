fe66e9a290bdefcce053f2716fe89563d5abb73d
Viet Anh <vietanh7@gmail.com>
2023-01-03 16:26:28 -0500

    Landscape Variations

---
+++ Landscape Variations/Test Variations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Variations
+//
+//  Created by NGUYEN VIET ANH on 2023-01-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
